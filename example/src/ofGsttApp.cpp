e13975ce80f3e09aa1524e37be5fe7e36887e8a8
Felix Lange <langefx@gmail.com>
2015-04-05 21:32:37 +0200

    readded missing source files

---
+++ example/src/ofGsttApp.cpp
@@ -0,0 +1,90 @@
+#include "ofGsttApp.h"
+
+//--------------------------------------------------------------
+void ofGsttApp::setup(){
+	ofSetFrameRate(60);
+	ofBackground(0, 0, 0);
+	ofSetLogLevel(OF_LOG_VERBOSE);
+	ofxSSL::appendData = false; //only needed to get show http errors
+
+	//init oF soundstream
+	int sampleRate = 44100;
+	int bufferSize = 256;
+	int nChannels = 1;
+	soundStream.printDeviceList();
+	soundStream.setDeviceID(3);
+	soundStream.setup(0, nChannels, sampleRate, bufferSize, 4);
+
+    //init ofxGSTT which starts listening here
+ 	gstt.setup(sampleRate,nChannels,"en-us",YOUR_KEY_AS_A_STRING,0.5f);
+	soundStream.setInput(gstt);
+
+ 	//register listener function to googles response events
+	ofAddListener(ofxGSTTTranscriptionThread::gsttApiResponseEvent,this,&ofGsttApp::gsttResponse);
+	bListening = true;
+
+	responseStr = "";
+}
+
+//--------------------------------------------------------------
+void ofGsttApp::update(){
+}
+
+void ofGsttApp::gsttResponse(ofxGSTTResponseArgs & response){
+	cout << "Response: " << response.msg << endl << "with confidence: " << ofToString(response.confidence) << endl;
+	cout << "processing time(ms): " << ofToString(response.tReceived - response.tSend) << endl;
+	if(response.msg != ""){
+		responseStr += response.msg + "\n";
+	}
+}
+
+//--------------------------------------------------------------
+void ofGsttApp::draw(){
+	ofBackground(0);
+	ofPushStyle();
+	ofSetColor(255,255,255);
+
+	if(gstt.isRecording()){
+		ofSetColor(255,0,0);
+	}else{
+		ofSetColor(20,20,20);
+	}
+	ofEllipse(ofGetWidth()/2,ofGetHeight()/2,200,200);
+
+	ofSetColor(255,255,255);
+	ofDrawBitmapString(responseStr,50,50);
+
+	ofPopStyle();
+}
+
+//--------------------------------------------------------------
+void ofGsttApp::keyPressed (int key){
+    switch(key){
+        case 'l':
+            bListening = !bListening;
+            gstt.setListening(bListening);
+            break;
+    }
+}
+
+//--------------------------------------------------------------
+void ofGsttApp::keyReleased  (int key){
+}
+
+//--------------------------------------------------------------
+void ofGsttApp::mouseMoved(int x, int y ){
+}
+
+//--------------------------------------------------------------
+void ofGsttApp::mouseDragged(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void ofGsttApp::mousePressed(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void ofGsttApp::mouseReleased(){
+
+}
+
---
+++ example/src/ofGsttApp.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGSTT.h"
+
+class ofGsttApp: public ofBaseApp{
+
+public:
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased();
+
+	void gsttResponse(ofxGSTTResponseArgs & response);
+
+	string responseStr;
+
+	ofxGSTT gstt;
+	ofSoundStream soundStream;
+	bool bListening;
+};
+
